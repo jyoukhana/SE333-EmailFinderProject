53b74a761021368fc70cee141f03b117ee2684ef
John Youkhana <jyoukhana1765@gmail.com>
2020-05-25 16:23:03 -0500

    created EmailFinderTest, added txt files for testing arguments

---
+++ src/test/java/edu/depaul/email/EmailFinderTest.java
@@ -0,0 +1,54 @@
+package edu.depaul.email;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class EmailFinderTest {
+
+    @Test
+    @DisplayName("Test EmailFinder output txt files with valid argument")
+    void outputTest(){
+        EmailFinder testFinder = new EmailFinder();
+        String[] testArgs = {"http://cdm.depaul.edu", "30"};
+        testFinder.run(testArgs);
+    }
+
+    @Test
+    @DisplayName("Tests if URL argument is valid")
+    void urlTest(){
+        EmailFinder testFinder = new EmailFinder();
+        String[] testArgs = {"validURL.txt"};
+        testFinder.run(testArgs);
+        String path = "good-links.txt";
+
+        try(Stream<String> txtLines = Files.lines(Paths.get(path))){
+            String res = txtLines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(res, "validURL.txt");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("Test EmailFinder with no URL argument")
+    void noURLTest(){
+        EmailFinder testFinder = new EmailFinder();
+        String[] testArgs = {};
+        testFinder.run(testArgs);
+        String path = "badlinks.txt";
+
+        try (Stream<String> txtLines = Files.lines(Paths.get(path))) {
+            String res = txtLines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(res, "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
